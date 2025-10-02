2a25a897c7570e014bc498c073d91c68d11694dd
StarlionGames <starliongames.dev@gmail.com>
2025-10-02 09:28:06 -0400

    finished!

--- Assignments/Assets/Challenge 1/Scripts/PlayerControllerX.cs
+++ Assignments/Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -24,6 +24,6 @@ public class PlayerControllerX : MonoBehaviour
         transform.Translate(Vector3.forward * speed);
 
         // tilt the plane up/down based on up/down arrow keys
-        //transform.Rotate(Vector3.right * rotationSpeed * Time.deltaTime);
+        transform.Rotate(new Vector3(-verticalInput,0, 0) * rotationSpeed * Time.deltaTime);
     }
 }
