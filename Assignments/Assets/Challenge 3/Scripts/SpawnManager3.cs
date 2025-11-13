5f328d73c353480714f82fc62f1c8860fbb8fd16
StarlionGames <starliongames.dev@gmail.com>
2025-11-13 09:45:43 -0500

    finished!

--- Assignments/Assets/Challenge 3/Scripts/SpawnManager3.cs
+++ Assignments/Assets/Challenge 3/Scripts/SpawnManager3.cs
@@ -13,12 +13,11 @@ public class SpawnManager3 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("PrawnsObject", spawnDelay, spawnInterval);
+        InvokeRepeating("SpawnObjects", spawnDelay, spawnInterval);
         playerControllerScript = GameObject.Find("Player").GetComponent<PlayerControllerX_3>();
     }
-
     // Spawn obstacles
-    void SpawnObjects ()
+    void SpawnObjects()
     {
         // Set random spawn location and random object index
         Vector3 spawnLocation = new Vector3(30, Random.Range(5, 15), 0);
